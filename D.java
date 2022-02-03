5b4900a41d20dae351e9dc12f15b44a314721a6c
Jhennifer Rodrigues <jhenniferrodrigues200119@gmail.com>
2022-02-03 19:40:18 -0300

    Criaçao da classe D

---
+++ D.java
@@ -0,0 +1,14 @@
+package Projeto_Jhennifer_Rodrigues;
+
+public class D {
+    private String D1;
+    private double D2;
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+}
